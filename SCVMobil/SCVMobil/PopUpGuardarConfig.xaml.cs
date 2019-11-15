be52b9c005cec6f7defe62d6ad36e0f87f597d63
Abraham Morillo <abrahammorillo777@gmail.com>
2019-11-14 23:33:52 -0400

    Fixing PopUp settings

--- SCVMobil/SCVMobil/PopUpGuardarConfig.xaml.cs
+++ SCVMobil/SCVMobil/PopUpGuardarConfig.xaml.cs
@@ -20,8 +20,11 @@ namespace SCVMobil
 
         private async void configsave_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new AppSettingsPage());
-            this.IsVisible = false;
+            this.Navigation.RemovePage(this.Navigation.NavigationStack[this.Navigation.NavigationStack.Count - 1]); //volver al appsettings//
+            this.IsVisible = false; //cerrar popup//
+            
+            //await Navigation.PushAsync(new AppSettingsPage());
+            //await this.Navigation.PopAsync();
         }
     }
 }
